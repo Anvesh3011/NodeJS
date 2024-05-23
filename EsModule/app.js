215d2b8f342106f345874d5ebbb9d1fb1d6a7c88
Anvesh_1130 <114339178+Anvesh3011@users.noreply.github.com>
2024-05-23 22:35:37 +0530

    Add files via upload

---
+++ EsModule/app.js
@@ -0,0 +1,6 @@
+import { name, bark, typeOfAnimal, age } from "./dog.js";
+
+console.log(`Type of Animal: ${typeOfAnimal}`);
+console.log(`Name: ${name}`);
+console.log(`Age: ${age}`);
+bark();
---
+++ EsModule/dog.js
@@ -0,0 +1,8 @@
+let typeOfAnimal = "Dog";
+let name = "Tommy";
+let age = 3;
+function bark() {
+  console.log("Bow Bow");
+}
+
+export { name, age, bark, typeOfAnimal };
