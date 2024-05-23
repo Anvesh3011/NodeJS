2bdc612b7f19bc2df252e973411762ccacd6dd64
Anvesh_1130 <114339178+Anvesh3011@users.noreply.github.com>
2024-05-23 22:34:27 +0530

    Add files via upload

---
+++ CommonJs/app.js
@@ -0,0 +1,6 @@
+var cat = require("./cat");
+
+console.log("Type of Animal: " + cat.typeOfAnimal);
+console.log("Name: " + cat.name);
+console.log("Age: " + cat.age);
+cat.meow();
---
+++ CommonJs/cat.js
@@ -0,0 +1,8 @@
+let typeOfAnimal = "Cat";
+let name = "Tom";
+let age = 2;
+function meow() {
+  console.log("Meow Meow");
+}
+
+module.exports = { name, age, meow, typeOfAnimal };
